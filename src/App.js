c3b1f7849bf2f980cb9e8725106a1188c42b6efa
emmanuel <condo98899@gmail.com>
2024-10-07 22:28:39 +0545

    my potfolio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Content'>
+        <h1>This is Content</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
